77b0cc05b7d2fec52e1f80843ad1c07eedf88b16
tonywin6 <1076227958@qq.com>
2019-09-10 11:28:34 +0800

    关键字识别+自动补全

--- brackets2/main.cpp
+++ brackets2/main.cpp
@@ -12,7 +12,15 @@ int main(int ac, char **av) {
     CompleterTextEdit *textEdit = new CompleterTextEdit;
     QStringList stringList;
     for (int index = 1; index <= 1; ++index) {
-        stringList << QString("()").arg(index)<<QString("{}").arg(index)<<QString("[]").arg(index);
+        stringList << QString("()")<<QString("{}")<<QString("[]")<<QString("char")
+                      <<QString("class")<<QString("const")<<QString("double")<<QString("enum")
+                      <<QString("enum")<<QString("explicit")<<QString("friend")<<QString("inline")
+                      <<QString("int")<<QString("long")<<QString("namespace")<<QString("operator")
+                      <<QString("private")<<QString("protected")<<QString("public")<<QString("short")
+                      <<QString("signals")<<QString("signed")<<QString("slots")<<QString("static")
+                      <<QString("struct")<<QString("template")<<QString("typedef")<<QString("typename")
+                      <<QString("union")<<QString("unsigned")<<QString("virtual")<<QString("void")
+                      <<QString("volatile")<<QString("bool");
     }
     QCompleter *completer = new QCompleter(stringList, textEdit);
     textEdit->setCompleter(completer);
